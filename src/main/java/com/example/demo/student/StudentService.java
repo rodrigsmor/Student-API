91c64537cacfaf0c435bef3e159df250eff88c27
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2021-09-14 17:52:31 -0300

    Post Mapping

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,10 +1,7 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
@@ -17,5 +14,9 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+
     }
 }
