ef09cbe5d9b9a7d2e865ee45fbc28296bfe49f04
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2021-09-14 18:22:22 -0300

    Validate email

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -17,6 +18,12 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.
+                findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
 
+        studentRepository.save(student);
     }
 }
